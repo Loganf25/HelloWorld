4a6f90f065478dfeeb87c9b16d7a17d0d2e37719
loganf25 <lflora@cub.uca.edu>
2024-08-27 14:02:46 -0500

    Lil recheck

--- src/Greeter.java
+++ src/Greeter.java
@@ -1,5 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Peerceptive Test Assignment
 public class Greeter {
     public static void main(String[] args) {
         System.out.print("Hello World!");
